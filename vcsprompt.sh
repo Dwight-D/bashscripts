fe8d3d1c8753cfd217f830a973e1298b471928ac
max.imhotep <forsman.max@gmail.com>
2018-09-14 10:26:43 +0200

    tweaked prompt

--- vcsprompt.sh
+++ vcsprompt.sh
@@ -1,33 +1,15 @@
 #!/bin/bash
 
 # Adapted from from https://github.com/hoelzro/bashrc/blob/master/colors.sh
+# Color variables set in .bashrc
 
 function __prompt
 {
-    # List of color variables that bash can use
-    local COL0="\[\033[0;30m\]"   # Black
-    local COL1="\[\033[1;30m\]"   # Dark Gray
-    local COL2="\[\033[0;31m\]"     # Red
-    local COL3="\[\033[1;31m\]"    # Light Red
-    local COL4="\[\033[0;32m\]"   # Green
-    local COL5="\[\033[1;32m\]"  # Light Green
-    local COL6="\[\033[0;33m\]"   # Brown
-    local COL7="\[\033[1;33m\]"  # Yellow
-    local COL8="\[\033[0;34m\]"    # Blue
-    local COL9="\[\033[1;34m\]"   # Light Blue
-    local COL10="\[\033[0;35m\]"  # Purple
-    local COL11="\[\033[1;35m\]" # Light Purple
-    local COL12="\[\033[0;36m\]"    # Cyan
-    local COL13="\[\033[1;36m\]"   # Light Cyan
-    local COL14="\[\033[0;37m\]"   # Light Gray
-    local COL15="\[\033[1;37m\]"   # White
-
-    local RESET="\[\033[0m\]"      # Color reset
-    local BOLD="\[\033[;1m\]"      # Bold
 
     # Base prompt
-    PS1="$COL13\h:$COL7\w$COL13 \\\$$RESET "
-
+    #PS1="$COL13\u $COL15 @ $COL9 \W\n$COL5>$COL_RESET "
+    PS1="$COL13\u $COL5 / $COL15 \W\n$COL5>$COL8 "
+    trap 'echo -ne "\e[0m" ' DEBUG
     local dirty
     local branch
 
@@ -58,11 +40,11 @@ function __prompt
     if [[ ! -z "$branch" ]]; then
         local status_color
         if [[ -z "$dirty" ]] ; then
-            status_color=$COL5
+            status_color=$BOLD$COL5
         else
             status_color=$COL3
         fi
-        PS1="$COL13($BOLD$status_color$branch$COL13)$RESET $PS1"
+        PS1="$COL15($status_color$branch$COL15)$RESET $PS1"
     fi
 }
 
