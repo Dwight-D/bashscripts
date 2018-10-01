c582da8fae9fbfccffa4619cff31d98b47372cd5
max.imhotep <forsman.max@gmail.com>
2018-10-01 10:12:06 +0200

    Updated initssh script to allow for direct ssh connection

--- initssh.sh
+++ initssh.sh
@@ -1,3 +1,16 @@
-#export TERM=xterm
-eval $(keychain --eval --quiet --agents ssh id_rsa)>/dev/null
+#!/bin/bash
+# Script for setting up terminal for SSH sessions
 
+#export TERM=xterm
+OLDTERM=$TERM
+if [ "$TERM" = "rxvt-unicode-256color" ]; then
+        TERM="rxvt-unicode"
+        export TERM
+        echo "Setting term to rxvt-unicode"
+fi
+eval $(keychain --eval --quiet --agents ssh id_rsa)>/dev/null
+if [ -n "$1" ]; then
+    ssh $1
+    TERM=$OLDTERM
+    echo Restoring term to $TERM
+fi
