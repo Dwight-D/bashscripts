2b1d4c9d5049c26a9f16f1ce280803024935f57d
max.imhotep <forsman.max@gmail.com>
2018-10-01 10:29:44 +0200

    Improved ssh script

--- initssh.sh
+++ initssh.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 # Script for setting up terminal for SSH sessions
 
-#export TERM=xterm
-OLDTERM=$TERM
+__OLDTERM=$TERM
+#Change to more recognizeable terminal
 if [ "$TERM" = "rxvt-unicode-256color" ]; then
         TERM="rxvt-unicode"
         export TERM
         echo "Setting term to rxvt-unicode"
 fi
+#Set up storing of SSH authentication using keychain
 eval $(keychain --eval --quiet --agents ssh id_rsa)>/dev/null
+#If argument supplied, connect directly over ssh and reset term after session
 if [ -n "$1" ]; then
-    ssh $1
-    TERM=$OLDTERM
+    #If connecting to TVAS, change to server-compliant terminal and disconnect subprocess
+    if [[ "$1" = "tvas"* ]]; then
+        nohup xterm -e ssh $1 &
+    else
+        ssh $1
+    fi
+    TERM=$__OLDTERM
     echo Restoring term to $TERM
 fi
